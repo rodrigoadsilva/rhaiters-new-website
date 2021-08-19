e51b1c6899568b13b1494bfe271751cc860b90ac
Rodrigo <raswolf23@gmail.com>
2021-08-19 17:59:41 -0300

    Ajustando o cover

--- js/Main.js
+++ js/Main.js
@@ -4,6 +4,14 @@ $(window).on('resize', function(){
  $('.banner').css({'height': (($(window).height()))+'px'});
 });
 
+const appHeight = () => {
+    const doc = document.documentElement
+    doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+}
+window.addEventListener('resize', appHeight)
+appHeight()
+
+
 /*$("#banner").animate({
     opacity: 0
 }, 1000, function() {
