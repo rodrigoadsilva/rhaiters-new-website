811d15632d8a64e3490e93928f3b5a4a7cda0e61
Rodrigo Silva <raswolf23@gmail.com>
2021-08-19 21:45:24 -0300

    Header is done

--- js/Main.js
+++ js/Main.js
@@ -1,15 +1,52 @@
+
+/*
+ * Ajusta o tamanho do cover
+ */
 $('.banner').css({'height': (($(window).height()))+'px'});
- 
 $(window).on('resize', function(){
  $('.banner').css({'height': (($(window).height()))+'px'});
 });
 
-const appHeight = () => {
-    const doc = document.documentElement
-    doc.style.setProperty('--app-height', `${window.innerHeight}px`)
-}
-window.addEventListener('resize', appHeight)
-appHeight()
+$(document).ready(function(){
+
+    // ********************* NAVBAR BEHAVIOR ****************************
+    // Close navbar on click
+    $('.navbar-nav>li>a').on('click', function(){
+        $('.navbar-collapse').collapse('hide');
+    })
+    // Check if navbar is toggle to change color
+    let navbarBlack = false;
+    $('#navbarToggle').on('click',function(){
+        if(!navbarBlack){
+            $('#navbarTop').css("background-color", "rgba(1, 1, 1, 0.8)");
+
+            navbarBlack = true;
+        }
+        else{
+            $('#navbarTop').css("background-color", "rgba(1, 1, 1, 0.2)");
+            navbarBlack = false;
+        }
+    });
+    // Check scroll to change navbar color
+    $(window).scroll(function (event) {
+        let scroll = $(window).scrollTop();
+        if(scroll >= 10){
+            $('#navbarTop').css("background-color", "rgba(1, 1, 1, 0.8)");
+            navbarBlack = true;
+        }
+        else{
+            $('#navbarTop').css("background-color", "rgba(1, 1, 1, 0.2)");
+            navbarBlack = false;
+        }
+    });
+    //*******************************************************************
+
+    //*********************** COVER BEHAVIOR ****************************
+    let coverChanger = setInterval(function(){
+        
+    });
+
+});
 
 
 /*$("#banner").animate({
