80944172ee0db614d14c7ad3685c2c82bcee40fc
Rodrigo Silva <raswolf23@gmail.com>
2021-08-25 22:55:49 -0300

    Services done, making reviews

--- js/Main.js
+++ js/Main.js
@@ -14,12 +14,24 @@ $(document).ready(function(){
     $('.navbar-nav>li>a').on('click', function(){
         $('.navbar-collapse').collapse('hide');
     })
+    // Close navbar on click outside
+    $(document).click(function () {
+        // if($(".navbar-collapse").hasClass("in")){
+          $('.navbar-collapse').collapse('hide');
+        // }
+     });
+     $(window).scroll(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
     // Check if navbar is toggle to change color
     let navbarBlack = false;
     $('#navbarToggle').on('click',function(){
+        let scroll = $(window).scrollTop();
+        if(scroll >= 10){
+            navbarBlack = false;
+        }
         if(!navbarBlack){
             $('#navbarTop').css("background-color", "rgba(1, 1, 1, 0.8)");
-
             navbarBlack = true;
         }
         else{
@@ -42,21 +54,27 @@ $(document).ready(function(){
     //*******************************************************************
 
     //*********************** COVER BEHAVIOR ****************************
+    let coverImage = 1;
     let coverChanger = setInterval(function(){
-        
-    });
-
+        console.log('Mudando o cover '+coverImage);
+        $("#banner").fadeTo('slow', 0.3, function(){
+            if(coverImage == 1){
+                $(this).toggleClass(`banner_${coverImage} banner_${coverImage+1}`);
+                $('#coverTitle').hide().html('<strong>Consultoria para o seu negócio</strong>').fadeIn('slow');
+                $('#coverSubtitle').hide().html('Quisque tincidunt semper gravida. Nunc ut leo quis ex maximus lacinia vitae sit amet orci. Nulla dapibus feugiat quam eu ultricies. Praesent id mollis turpis. Curabitur tellus eros, luctus sit amet lectus ac, facilisis lobortis quam.').fadeIn('slow');
+            }
+            else if(coverImage == 2){
+                $(this).toggleClass(`banner_${coverImage} banner_${coverImage+1}`);
+                $('#coverTitle').hide().html('<strong>E uma placa-mãe de fundo</strong>').fadeIn('slow');
+                $('#coverSubtitle').hide().html('Aenean sollicitudin volutpat ornare. Donec magna sem, aliquam eu est sit amet, gravida dignissim enim. Nulla tempus odio arcu, eu venenatis nunc scelerisque vel. ').fadeIn('slow');
+            }
+            else{
+                $(this).toggleClass(`banner_${coverImage} banner_${coverImage-2}`);
+                $('#coverTitle').hide().html('<strong>Especialistas em tecnologia</strong>').fadeIn('slow');
+                $('#coverSubtitle').hide().html('Disponibilizando para sua empresa especialistas prontamente aptos a entregar soluções de alta qualidade').fadeIn('slow');
+            }
+            coverImage++;
+            if(coverImage == 4){coverImage = 1;}
+        }).fadeTo('slow', 1);
+    }, 6000);
 });
-
-
-/*$("#banner").animate({
-    opacity: 0
-}, 1000, function() {
-    // Callback
-    $(this).css("background", `url("./assets/images/cover/originals/cover_1.jpg") fixed center no-repeat`).promise().done(function(){
-        // Callback of the callback :)
-        $(this).animate({
-            opacity: 1
-        }, 600)
-    });    
-});*/
